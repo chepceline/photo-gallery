b1cc8d6654bce85b83890d901addd218408b6921
chepceline <chepceline25@gmail.com>
2022-05-29 17:08:15 +0300

    add static urls

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -124,6 +124,10 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
