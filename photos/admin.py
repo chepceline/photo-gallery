83a81216e48f38762b6a5e66399daa01d6689358
chepceline <chepceline25@gmail.com>
2022-05-31 08:10:03 +0300

    initial

--- photos/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import Photo, Category, Location
-
-admin.site.register(Category)
-admin.site.register(Photo)
-admin.site.register(Location)
